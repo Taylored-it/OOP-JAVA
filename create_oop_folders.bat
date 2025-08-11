b435d0b37c3de5d8cb810a273a606d693a6b3802
Taylored-it <157323174+Taylored-it@users.noreply.github.com>
2025-08-11 09:45:55 +0200

    Add files via upload

---
+++ create_oop_folders.bat
@@ -0,0 +1,67 @@
+@echo off
+REM Create OOP placeholder folders with tmp.txt in each
+
+mkdir oop
+
+mkdir oop\01_whatis
+echo. > oop\01_whatis\tmp.txt
+
+mkdir oop\02_reallife
+echo. > oop\02_reallife\tmp.txt
+
+mkdir oop\03_constructors
+echo. > oop\03_constructors\tmp.txt
+
+mkdir oop\04_chaining
+echo. > oop\04_chaining\tmp.txt
+
+mkdir oop\05_inheritance
+echo. > oop\05_inheritance\tmp.txt
+
+mkdir oop\06_super
+echo. > oop\06_super\tmp.txt
+
+mkdir oop\06_1_challenge_0
+echo. > oop\06_1_challenge_0\tmp.txt
+
+mkdir oop\07_static_vs_instance
+echo. > oop\07_static_vs_instance\tmp.txt
+
+mkdir oop\08_initializers
+echo. > oop\08_initializers\tmp.txt
+
+mkdir oop\09_interfaces
+echo. > oop\09_interfaces\tmp.txt
+
+mkdir oop\10_beans
+echo. > oop\10_beans\tmp.txt
+
+mkdir oop\11_params
+echo. > oop\11_params\tmp.txt
+
+mkdir oop\12_1_exam1
+echo. > oop\12_1_exam1\tmp.txt
+
+mkdir oop\12_2_exam2
+echo. > oop\12_2_exam2\tmp.txt
+
+mkdir oop\12_3_exam3
+echo. > oop\12_3_exam3\tmp.txt
+
+mkdir oop\12_4_exam4
+echo. > oop\12_4_exam4\tmp.txt
+
+mkdir oop\12_5_exam5
+echo. > oop\12_5_exam5\tmp.txt
+
+mkdir oop\12_6_exam6
+echo. > oop\12_6_exam6\tmp.txt
+
+mkdir oop\12_7_exam7
+echo. > oop\12_7_exam7\tmp.txt
+
+mkdir oop\13_javadoc
+echo. > oop\13_javadoc\tmp.txt
+
+echo All folders and tmp.txt files created successfully!
+pause
